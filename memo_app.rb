fa100261325d09e14db738fce7455d3f0ea98e2b
Ubuntu <ubuntu@ip-172-31-35-154.ap-northeast-1.compute.internal>
2023-06-16 11:13:45 +0000

    Ruby 1 再提出

--- memo_app.rb
+++ memo_app.rb
@@ -1,65 +1,43 @@
 require 'csv'
 
 def create_memo
-  print "メモのタイトルを入力してください: "
-  title = gets.chomp
-  print "メモの内容を入力してください: "
-  content = gets.chomp
+  print "拡張子を除いたファイルを入力してください: "
+  file_name = gets.chomp
 
-  CSV.open('memos.csv', 'a') do |csv|
-    csv << [title, content]
+  print "メモしたい内容を記入してください\n完了したら Ctrl + D をおします\n"
+  contents = STDIN.read.chomp
+
+  CSV.open("#{file_name}.csv", 'w') do |csv|
+    csv << [contents]
   end
 
   puts "メモが保存されました。"
 end
 
-
-
-
 def edit_memo
-  memos = []
+  print "編集したいファイルを入力してください: "
+  file_name = gets.chomp
 
-  puts "編集するメモの番号を入力してください:"
-  CSV.foreach('memos.csv') do |row|
-    memos << row
-  end
+  if File.exist?("#{file_name}.csv")
+    print "メモしたい内容を記入してください\n完了したら 
+    を入力してください\n"
+    contents = STDIN.read.chomp
 
-  memos.each_with_index do |memo, index|
-    puts "#{index + 1}. #{memo[0]}"
-  end
-
-  print "選択してください: "
-  memo_number = gets.chomp.to_i
-
-  if memo_number > 0 && memo_number <= memos.length
-    memo_to_edit = memos[memo_number - 1]
-
-    print "新しいタイトルを入力してください (現在のタイトル: #{memo_to_edit[0]}): "
-    new_title = gets.chomp
-    print "新しい内容を入力してください (現在の内容: #{memo_to_edit[1]}): "
-    new_content = gets.chomp
-
-    memos[memo_number - 1] = [new_title, new_content]
-
-    CSV.open('memos.csv', 'w') do |csv|
-      memos.each do |memo|
-        csv << memo
-      end
+    CSV.open("#{file_name}.csv", 'w') do |csv|
+      csv << [contents]
     end
 
-    puts "#{memo_to_edit[0]} のメモを編集しました。"
+    puts "メモを編集しました。"
   else
-    puts "無効な選択です。"
+    puts "指定されたファイルは存在しません。"
   end
 end
-
-
 
 def run
   loop do
     puts "メモアプリ"
-    puts "1. メモを作成する"
-    puts "2. メモを編集する"
+    puts "1. 新規メモを作成する"
+    puts "2. 既存のメモを編集する"
     puts "3. 終了する"
     print "選択してください: "
     choice = gets.chomp.to_i
@@ -80,4 +58,3 @@ def run
 end
 
 run
-
